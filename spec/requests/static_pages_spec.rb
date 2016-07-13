99480f35c23f85ac4782e83faaec92c8360024f6
System Administrator <root@Abhi.local>
2016-07-13 12:51:56 +0530

    Finish Contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -33,4 +33,16 @@ describe "Static pages" do
        	 									:text => "Ruby on Rails Tutorial Sample App | About Us")
        	end
     end
+
+    describe "Contact page" do
+    	it "should have the content 'Contact" do
+    		visit "/static_pages/contact"
+    		page.should have_selector('h1',:text => 'Contact')
+    	end
+
+    	it "should have title 'Contact Us'" do
+    		visit "/static_pages/contact"
+    		page.should have_selector('title',:text => "Ruby on Rails Tutorial Sample App | Contact Us")
+    	end
+    end
 end
