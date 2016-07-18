4776d818971ec7e1105e719452f582b0b4dd6b6f
Abhinaya_Pabbisetty <Abhinaya_Pabbisetty@Abhi.local>
2016-07-18 13:12:35 +0530

    First User Signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,16 +1,49 @@
 require 'spec_helper'
 
 describe "UserPages" do
+  subject { page }	
   describe "Sign up" do
-      before { visit signup_path }
+      before { visit new_user_path }
 
       it "should have heading sign up" do
-      	page.should have_selector('h1', text: 'Sign Up')
+      	 should have_selector('h1', text: 'Sign Up')
       end
 
       it "should have title including sign up" do
-      	page.should have_selector('title',text: "Ruby on Rails Tutorial Sample App | Sign Up")
+      	should have_selector('title',text: "Ruby on Rails Tutorial Sample App | Sign Up")
       end
     
+  end
+
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) } 
+    before { visit user_path(user) }
+    it { should have_selector('h1', text: user.name) }
+    it { should have_selector('title', text: user.name) } 
+  end
+
+  describe "user sign in page" do
+    describe "when empty form is submitted" do
+      before { visit new_user_path }
+      it "should not create a user" do
+        expect do
+          click_button "Create my account"
+        end.not_to change(User,:count)
+      end
+    end
+
+    describe "when submitting a valid form" do
+      before { visit new_user_path 
+      fill_in "Name", with: "Example user"
+      fill_in "Email", with: "user@example.com"
+      fill_in "Password", with: "foobar"
+      fill_in "Confirmation", with: "foobar"
+    }
+    it "should create a user" do
+      expect do
+      click_button "Create my account"
+      end.to change(User,:count).by(1)
+    end
+    end
   end
 end
